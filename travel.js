8a34da4beca7ec2fe0033ab1f9d70a821d0627e3
trhbi <RIKAKO@net44-dhcp113.sfc.keio.ac.jp>
2015-07-09 12:18:15 +0900

    選択した月を反映！

--- travel.js
+++ travel.js
@@ -31,25 +31,29 @@ function OnButtonClick() {
 var APIKEY = "ffb6062385e7eb4f";
  var endpoint = "http://webservice.recruit.co.jp/ab-road-air/ticket/v1/";
 
-function createCondition(deperture, destination){
+function createCondition(deperture, destination,month){
   return {
     key: APIKEY,
     city: destination,
     dept_detail: deperture,
-    ym:07,
+    ym:month,
     format: "jsonp"
   };
 }
 
 function dataLoaded(data){
   console.log(data);
-  alert(data.results.ticket[0].price.min + "円")
+  document.write(data.results.ticket[0].price.min + "円");
+  document.write(data.results.ticket[0].airline_summary);
 }
 
 function search(){
+  var a = document.getElementById("month").value;
+  console.log(a);
+
   jQuery.ajax({
     url: endpoint,
-    data: createCondition("HND", "ANK"),
+    data: createCondition("HND", "IST", a),
     dataType: "jsonp",
     success:dataLoaded
   });
